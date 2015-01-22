d6fb6e7afae77f99eb183488222171eed889ecdb
John Babak <babak.john@gmail.com>
2015-01-22 05:43:53 +0300

    Removed persistence tests, persistence was removed

--- test/integration-smoke-test.js
+++ test/integration-smoke-test.js
@@ -31,38 +31,12 @@ define(['../lib/loglevel', 'test/test-helpers'], function(log, testHelpers) {
         });
 
         describeIf(typeof console !== "undefined", "log levels", function() {
-            beforeEach(function() {
-                this.addMatchers({
-                    "toBeTheStoredLevel" : testHelpers.toBeTheStoredLevel
-                });
-            });
-
             it("are all settable", function() {
                 log.setLevel(log.levels.TRACE);
                 log.setLevel(log.levels.DEBUG);
                 log.setLevel(log.levels.INFO);
                 log.setLevel(log.levels.WARN);
                 log.setLevel(log.levels.ERROR);
-            });
-
-            itIf(testHelpers.isAnyLevelStoragePossible(), "are persisted", function() {
-                log.setLevel(log.levels.TRACE);
-                expect('trace').toBeTheStoredLevel();
-
-                log.setLevel(log.levels.DEBUG);
-                expect('debug').toBeTheStoredLevel();
-
-                log.setLevel(log.levels.INFO);
-                expect('info').toBeTheStoredLevel();
-
-                log.setLevel(log.levels.WARN);
-                expect('warn').toBeTheStoredLevel();
-
-                log.setLevel(log.levels.ERROR);
-                expect('error').toBeTheStoredLevel();
-
-                log.setLevel(log.levels.SILENT);
-                expect('silent').toBeTheStoredLevel();
             });
         });
     });
